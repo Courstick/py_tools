e698a1a9c07d2835b2e7260bbdde89b6c2d60e5d
Courstick <52244612+Courstick@users.noreply.github.com>
2025-07-16 18:11:43 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
-# -*- coding: utf-8 -*-
-import time
 from contextlib import contextmanager
-
+from functools import wraps
 
 @contextmanager
-def timer(name):
+def timing(name):
     """decorator used to timing"""
     start = time.time()
     yield
     finish = time.time()
     print(f'{name} took {finish - start:.4f} sec')
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with timing(func.__name__):
+            return func(*args, **kwargs)
+    return wrapper
